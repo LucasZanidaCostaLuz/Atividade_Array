b5723dc038822222eaacc70a7c2ee1722ef7bc29
Lucas Zani <zanin0503@gmail.com>
2024-03-17 20:19:09 -0300

    quarto metodo - Push

---
+++ Metodo4_Array.js
@@ -0,0 +1,25 @@
+//Exemplo 1
+//criei uma variável composta e dei valores a ela
+let jogos = ['clash royale', 'clash of clans', 'brawl stars'];
+
+//usei o console.log para exibir os valores que eu dei a ela
+console.log(jogos);
+
+//usei o push para adicionar mais valores a váriavel
+jogos.push('god of war', 'dragon city', 'PvZ');
+
+//usei o console.log para exibir que eu somei mais valores a ela
+console.log(jogos);
+
+//Exemplo 2
+//criei uma variável composta e dei valores a ela
+let carros = ['McQueen', 'Mater', 'Chick Hicks'];
+
+//usei o console.log para exibir os valores que eu dei a ela
+console.log(carros);
+
+//usei o push para adicionar mais valores a váriavel
+carros.push('Guido', 'Mack', 'Ramone');
+
+//usei o console.log para exibir que eu somei mais valores a ela
+console.log(carros);
