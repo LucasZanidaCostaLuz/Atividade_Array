3d7c3f9628312cb3c3ff39b9168e0ae9722f3fd9
Lucas Zani <zanin0503@gmail.com>
2024-03-17 19:41:36 -0300

    primeiro metodo - Length

---
+++ Metodo1_Array.js
@@ -0,0 +1,19 @@
+//Exemplo 1
+//criei a variável estados e dei valores a elas
+let Estados = ['Amapá', 'Ceará', 'Rondônia', 'Maranhão'] 
+
+//criei a variável quantidadeEstados e coloquei o valor dela como o comprimento da variável composta Estados
+let quantidadeEstados = Estados.length 
+
+//coloquei o console.log, que fara a função de exibir o valor do comprimento da variável composta Estados
+console.log(quantidadeEstados);
+
+//Exemplo 2
+//criei a variável marcaChocolate e dei valores a elas
+let marcaChocolate = ['Lacta', 'Garoto', 'Nestle', 'Snickers'] 
+
+//criei a variável quantidadeMarca e coloquei o valor dela como o comprimento da variável composta marcaChocolate
+let quantidadeMarca = marcaChocolate.length 
+
+//coloquei o console.log, que fara a função de exibir o valor do comprimento da variável composta Estados
+console.log(quantidadeMarca);
