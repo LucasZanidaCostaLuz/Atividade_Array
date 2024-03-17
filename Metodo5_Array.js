4b65cf99bfa4f5605757fb1af3ead80c13a17e7f
Lucas Zani <zanin0503@gmail.com>
2024-03-17 20:42:52 -0300

    quinto metodo - Concat

---
+++ Metodo5_Array.js
@@ -0,0 +1,25 @@
+//Exemplo 1
+//criei e atribui valores a váriavel frutas
+let frutas = ['maçã', 'banana', 'morango'];
+
+//criei e atribui valores a váriavel whey
+let whey = ['baunilha', 'mousse de maracuja', 'milkshake de chocolate'];
+
+//criei a variável mistura e atribui a ela o valor da variável frutas mais a variável whey
+let frutasComWhey = frutas.concat(whey);
+
+//exibi o valor da variável mistura
+console.log(frutasComWhey);
+
+//Exemplo 2
+//criei e atribui valores a váriavel comidaBrasileira
+let comidaBrasileira = ['feijoada', 'churrasco', 'arroz com feijão'];
+
+//criei e atribui valores a váriavel comidaEstadunidense
+let comidaEstadunidense = ['Panquecas', 'Waffle', 'ovos com bacons'];
+
+//criei a variável comidas e atribui a ela o valor da variável comidaBrasileira mais a variável comidaEstadunidense
+let comidas = comidaBrasileira.concat(comidaEstadunidense);
+
+//exibi o valor da variável comidas
+console.log(comidas);
