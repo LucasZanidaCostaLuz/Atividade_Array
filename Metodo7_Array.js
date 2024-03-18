f4494caadfdb4a209ba15af03295c9195bb69fa7
Lucas Zani <zanin0503@gmail.com>
2024-03-17 21:59:12 -0300

    metodo 7 - Reverse

---
+++ Metodo7_Array.js
@@ -0,0 +1,25 @@
+//Exemplo 1
+//criei a variável posiçãoCorrida e atribuí a ela um valor
+let posiçãoCorrida = ['primeiro', 'segundo', 'terceiro'];
+
+//usei o console.log para exibir o valor da posiçãoCorrida
+console.log('posição da corrida :', posiçãoCorrida);
+
+//criei uma variável posiçãoContrária e atribuí a ela o valor inverso a posiçãoCorrida
+let posiçãoContraria = posiçãoCorrida.reverse();
+
+//exibi o valor da posiçãoContraria
+console.log('posição inversa da corrida:', posiçãoContraria);
+
+//Exemplo 2
+//criei a variável posiçãoCorrida e atribuí a ela um valor
+let melhoresAtores = ['Heath Ledger', 'cillian murphy', 'willem dafoe'];
+
+//usei o console.log para exibir o valor da posiçãoCorrida
+console.log('Melhores atores em ordem:', melhoresAtores);
+
+//criei uma variável posiçãoContrária e atribuí a ela o valor inverso a posiçãoCorrida
+let melhoresAtoresOrdemContraria = melhoresAtores.reverse();
+
+//exibi o valor da posiçãoContraria
+console.log('melhores atores em ordem contraria :', melhoresAtoresOrdemContraria);
